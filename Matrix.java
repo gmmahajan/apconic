c58fb05ced1c3c1228357ace4a67f59d30e47c41
gmmahajan <gmahajan93@gmail.com>
2015-06-15 18:37:34 +0530

    java program
    program for matrix input

---
+++ Matrix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Matrix
+{
+  public static void main(String arg[])
+{
+  Scanner KB=new Scanner(System.in);
+  System.out.print("enter rows ");
+  int r=KB.nextInt();
+  System.out.print("enter colms ");
+  int c=KB.nextInt();
+  int x[][]=new int[r][c];
+  for(int i=0;i<x.length;i++)
+  {
+  for(int j=0;j<x.length;j++)
+  {
+  System.out.print("x["+i+"]["+j+"]");
+  x[i][j]=KB.nextInt();
+  }
+  }
+ for(int i=0;i<x.length;i++)
+   {
+  for(int j=0;j<x[i].length;j++)
+  {
+  System.out.print(x[i][j]);
+  }
+  }
+  System.out.println();
+  }
+  }
